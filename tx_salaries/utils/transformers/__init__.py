7bd6ceb2632034b3a954ea2ab5d529bc423d626e
Travis Swicegood <development@domain51.com>
2013-10-08 11:39:51 -0500

    Extract all of the transformer code into its own module
    There's an import issue when `transformers` contains all of the
    transformation logic.  I've moved it to its own module to avoid.  This
    change keeps all of the individual transformers in their own module and
    all of the transformation code in a separate module.
    
    This is definitely a candidate for renaming to avoid the singular-plural
    confusion that could happen.

--- tx_salaries/utils/transformers/__init__.py
+++ tx_salaries/utils/transformers/__init__.py
@@ -1,53 +1,7 @@
-import hashlib
-from StringIO import StringIO
-
-from csvkit import convert
-from csvkit.unicsv import UnicodeCSVReader
-from django.core import exceptions
-
 from . import alamo_colleges
 from . import brownsville_isd
 from . import collin_college
 from . import el_paso_county
-
-
-def convert_to_csv_reader(filename):
-    format = convert.guess_format(filename)
-    f = open(filename, "rb")
-    converted = StringIO(convert.convert(f, format))
-    reader = UnicodeCSVReader(converted)
-    return reader
-
-
-def transform(filename):
-    reader = convert_to_csv_reader(filename)
-    labels = reader.next()
-    transformers = get_transformers(labels)
-
-    if len(transformers) > 1:
-        raise Exception("TODO")
-
-    transformer = transformers[0]
-    # TODO: Figure out a better way to pass a dict reader in
-    data = transformer(labels, reader)
-    return data
-
-
-def generate_key(labels):
-    return hashlib.sha1("::".join(labels)).hexdigest()
-
-
-def get_transformers(labels):
-    """
-    Return one (or more) transfer for a given set of headers
-
-    This takes a list of headers for a given spreadsheet and returns
-    the known transformers that match against it.
-    """
-    try:
-        return TRANSFORMERS[generate_key(labels)]
-    except KeyError:
-        raise exceptions.ImproperlyConfigured()
 
 
 TRANSFORMERS = {
