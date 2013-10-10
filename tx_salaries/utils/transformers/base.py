69a260fb7177e13b2a3ccca553ac3d8b4295298b
Travis Swicegood <development@domain51.com>
2013-10-10 11:29:08 -0500

    Quick and dirty way to generate an ID for each person
    This doesn't allow tracking people across name changes for sure, and if
    you include departments and job titles, we lose the ability to track
    them across department moves and title changes.
    
    The idea is to start asking departments for some sort of identifier that
    is unique to a person and will stay with them as they move around.
    Hopefully we're able to get that, but that won't allow moves into
    completely different areas (El Paso County to City of El Paso or UTEP,
    for example).

--- tx_salaries/utils/transformers/base.py
+++ tx_salaries/utils/transformers/base.py
@@ -1,3 +1,7 @@
+from copy import copy
+import hashlib
+import re
+
 DEFAULT_DATA_TEMPLATE = {
     'tx_people.Person': {},
     'tx_people.Organization': {},
@@ -6,3 +10,25 @@ DEFAULT_DATA_TEMPLATE = {
     'tx_salaries.CompensationType': {},
     'tx_salaries.Employee': {},
 }
+
+
+def create_hash_for_row(row, exclude=None):
+    """
+    Returns a hash for a row to be used as its identifier.
+
+    This takes a dictionary called ``row`` and turns it into a SHA-1
+    hash while excluding any fields that might vary (such as pay rate).
+    Each data source is responsible for determining what fields are
+    unique and immutable.
+
+    This hash is used for tracking an employee from one import to the
+    next.
+    """
+
+    data_for_hash = copy(row)
+    if exclude:
+        for key in exclude:
+            del data_for_hash[key]
+
+    hash_string = re.sub('[\s.,-]', '', "::".join(data_for_hash.values()))
+    return hashlib.sha1(hash_string).hexdigest()
