54e917fa3ccbd0ec7c17ebbcdb386084b91e544d
Travis Swicegood <development@domain51.com>
2013-10-22 14:40:49 -0500

    Add in the first pass at the Texas Tech System

--- tx_salaries/utils/transformers/__init__.py
+++ tx_salaries/utils/transformers/__init__.py
@@ -2,6 +2,7 @@ from . import alamo_colleges
 from . import brownsville_isd
 from . import collin_college
 from . import el_paso_county
+from . import texas_tech_system
 from . import texas_tech_university
 
 
@@ -10,5 +11,6 @@ TRANSFORMERS = {
     '6e4bb90321c62a34296cad61c3800b1dd308257e': [brownsville_isd.transform, ],
     '39bcb735ae0a6d3b9bddb839337680abf76be1fd': [collin_college.transform, ],
     'd18ad5c9635a40de15b0ead0e8f8fe97b8ccca31': [el_paso_county.transform, ],
+    'bc023bcf9e573ecdf8335ebafb24feec596e473f': [texas_tech_system.transform, ],
     'c60489252da989ab04f6a47a86265dc047dd367b': [texas_tech_university.transform, ],
 }
