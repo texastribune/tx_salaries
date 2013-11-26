a8fd0bebdf7d7e71f6bbe58d657b26ade670bcf7
Travis Swicegood <development@domain51.com>
2013-11-26 14:15:45 -0600

    No need for this to be on a separate line

--- tx_salaries/managers.py
+++ tx_salaries/managers.py
@@ -8,8 +8,7 @@ class PositionStatsManager(models.Manager):
         Employee = obj._meta.concrete_model
         position_cohort = Employee.objects.filter(
                 position__organization=obj.position.organization)
-        stats, created = self.get_or_create(
-                position=obj.position.post)
+        stats, created = self.get_or_create(position=obj.position.post)
         stats.highest_paid = position_cohort.order_by('-compensation')[0]
         stats.lowest_paid = position_cohort.order_by('compensation')[0]
         stats.save()
