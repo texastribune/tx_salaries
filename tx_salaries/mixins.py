d6bd21348c0758404e7cb884c639a7c618f4e281
Travis Swicegood <development@domain51.com>
2013-11-26 14:15:56 -0600

    Add first pass at docs

--- tx_salaries/mixins.py
+++ tx_salaries/mixins.py
@@ -2,6 +2,13 @@ from tx_people.mixins import *
 
 
 class DenormalizeOnSaveMixin(object):
+    """
+    Adjusts a model's save method to denormalize data where it can
+
+    This works by going through all related objects and calling its
+    ``denormalize`` manager method.  Each of those methods should take
+    one argument -- the model that's being saved.
+    """
     def save(self, denormalize=True, *args, **kwargs):
         obj = super(DenormalizeOnSaveMixin, self).save(*args, **kwargs)
         # TODO: Abstract into a general library
