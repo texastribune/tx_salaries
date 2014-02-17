c9dccf6845256c5cf5c2583ceb9d3e7f6ba30bfb
Travis Swicegood <development@domain51.com>
2014-02-17 15:16:21 -0600

    Bump to dev version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ for dirpath, dirnames, filenames in os.walk('tx_salaries'):
 
 setup(
     name='tx_salaries',
-    version='0.2.0',
+    version='0.3alpha.0',
     description='Texas Tribune: tx_salaries',
     author='Tribune Tech',
     author_email='tech@texastribune.org',
