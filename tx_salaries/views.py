45694cfc731b5d30cf9da38a9435ab6f2c6d66a3
danhillreports <danhillreports@gmail.com>
2014-04-16 12:41:18 -0500

    Add employee-peer views and styles to match Ryan's mocks

--- tx_salaries/views.py
+++ tx_salaries/views.py
@@ -7,6 +7,18 @@ from . import models
 class EmployeeView(TemplateView):
     template_name = 'tx_salaries/employee.html'
 
+    gender_map = {'M': 'Male', 'F': 'Female'}
+
+    def get_top_peers(self, employee):
+        return (models.Employee.objects
+                                .filter(position__post=employee.position.post)
+                                .order_by('-compensation')[:5])
+
+    def get_top_dept(self, employee):
+        return (models.Employee.objects
+                                .filter(position__post__organization=employee.position.post.organization)
+                                .order_by('-compensation')[:5])
+
     def get_context_data(self, **kwargs):
         context = super(EmployeeView, self).get_context_data(**kwargs)
         p = models.Employee.objects.get(id=self.kwargs['employee_id'])
@@ -15,11 +27,13 @@ class EmployeeView(TemplateView):
             'title': p.title.name,
             'department': p.position.organization.name,
             'agency': p.position.organization.parent.name,
-            'gender': p.position.person.gender,
+            'gender': self.gender_map[p.position.person.gender],
             'hire_date': p.hire_date,
             'salary': p.compensation,
-            'ft': p.compensation_type
+            'ft': p.compensation_type,
         }
+        context['peers'] = self.get_top_peers(p)
+        context['top_dept'] = self.get_top_dept(p)
         return context
 
 
