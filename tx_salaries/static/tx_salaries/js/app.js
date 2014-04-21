e2a894b6f1ad30589ec4d5971ebbf8d5759f73ce
danhillreports <danhillreports@gmail.com>
2014-04-21 17:26:56 -0500

    Add js file

---
+++ tx_salaries/static/tx_salaries/js/app.js
@@ -0,0 +1,31 @@
+var $navPos = $('.nav').position().top;
+var $nav = $('.nav');
+
+$(window).scroll(function() {
+  var $scrollTop = $(window).scrollTop();
+  $('.active').removeClass('active');
+  $fixRail($scrollTop);
+  $highlightArrows($scrollTop);
+});
+
+
+var $highlightArrows = function(scrollTop) {
+  if (scrollTop > $('#employment').position().top) {
+    $('#employment-arrow').addClass('active');
+  } else if (scrollTop > $('#ethnicity').position().top) {
+    $('#ethnicity-arrow').addClass('active');
+  } else if (scrollTop > $('#gender').position().top) {
+    $('#gender-arrow').addClass('active');
+  } else {
+    $('#overview-arrow').addClass('active');
+  }
+};
+
+var $fixRail = function(scrollTop) {
+  if (scrollTop > $navPos) {
+    $nav.addClass('fixed-rail');
+    $nav.css('width', $('.overview_header').width());
+  } else {
+    $nav.removeClass('fixed-rail');
+  }
+};
