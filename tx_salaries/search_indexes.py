bf863b3d09902e1dee0357689edd6c3aee489c51
danhillreports <danhillreports@gmail.com>
2014-05-12 12:45:30 -0500

    Add EmployeeIndex facets

--- tx_salaries/search_indexes.py
+++ tx_salaries/search_indexes.py
@@ -14,6 +14,10 @@ class OrganizationIndex(indexes.SearchIndex, indexes.Indexable):
 class EmployeeIndex(indexes.SearchIndex, indexes.Indexable):
     text = indexes.CharField(document=True, use_template=True)
     content_auto = indexes.EdgeNgramField(model_attr='position__person__given_name')
+    compensation = indexes.IntegerField(model_attr='compensation', null=True)
+    title = indexes.CharField(model_attr='title__name', faceted=True)
+    organization = indexes.CharField(model_attr='position__organization__name', faceted=True)
+    parent = indexes.CharField(model_attr='position__organization__parent__name', faceted=True)
 
     def get_model(self):
         return Employee
