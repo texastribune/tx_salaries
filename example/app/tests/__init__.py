185f708729fa9959c19f952f55d94f3e46ce8d5d
danhillreports <danhillreports@gmail.com>
2014-05-20 13:24:01 -0500

    Move tests to mgmt cmd test dir

--- example/app/tests/__init__.py
+++ example/app/tests/__init__.py
@@ -3,6 +3,4 @@ from .models import *
 from .query import *
 from .utils import *
 from .views import *
-from .denormalize_salary_data import  *
-
-# TODO move management command
+from .management import *
---
+++ example/app/tests/management/commands/__init__.py
@@ -0,0 +1,1 @@
+from .denormalize_salary_data import *
