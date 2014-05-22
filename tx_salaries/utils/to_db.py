0466e1b725adcb792d2a2ddd9a5d8f2ca039303e
Ryan Murphy <github@accounts.rdmurphy.org>
2014-05-21 19:58:50 -0500

    More indent changes for flake8 happiness

--- tx_salaries/utils/to_db.py
+++ tx_salaries/utils/to_db.py
@@ -7,7 +7,7 @@ def save(data):
     save_for_stats = {'organizations': [], 'positions': []}
     # TODO: Save source data
     identifier, id_created = tx_people.Identifier.objects.get_or_create(
-            **data['tx_people.Identifier'])
+        **data['tx_people.Identifier'])
 
     race, _ = tx_people.Race.objects.get_or_create(**data['tx_people.Race'])
 
@@ -24,27 +24,27 @@ def save(data):
     # TODO: How should we/can we deal with recursive children?
     children = data['tx_people.Organization'].pop('children', [])
     source_department, _ = tx_people.Organization.objects.get_or_create(
-            **data['tx_people.Organization'])
+        **data['tx_people.Organization'])
     save_for_stats['organizations'].append(source_department)
     for child in children:
         department, _ = tx_people.Organization.objects.get_or_create(
-                parent=source_department, **child)
+            parent=source_department, **child)
         save_for_stats['organizations'].append(department)
 
     # TODO: Remove post entirely
     post, _ = tx_people.Post.objects.get_or_create(organization=department,
-            **data['tx_people.Post'])
+                                                   **data['tx_people.Post'])
     save_for_stats['positions'].append(post)
 
     membership, _ = tx_people.Membership.objects.get_or_create(
-            person=person, organization=department, post=post,
-            **data['tx_people.Membership'])
+        person=person, organization=department, post=post,
+        **data['tx_people.Membership'])
 
     for compensation in data['compensations']:
         compensation_type, _ = models.CompensationType.objects.get_or_create(
-                **compensation['tx_salaries.CompensationType'])
+            **compensation['tx_salaries.CompensationType'])
         title, _ = models.EmployeeTitle.objects.get_or_create(
-                **compensation['tx_salaries.EmployeeTitle'])
+            **compensation['tx_salaries.EmployeeTitle'])
         models.Employee.objects.get_or_create(
             position=membership, compensation_type=compensation_type,
             title=title, **compensation['tx_salaries.Employee'])
