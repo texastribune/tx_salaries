4a7ac5d6a33f17cb6afa2617d90ac9be857cc14e
danhillreports <danhillreports@gmail.com>
2014-05-29 15:20:15 -0500

    Test even median on top-level stats

--- example/app/tests/managers.py
+++ example/app/tests/managers.py
@@ -5,8 +5,8 @@ from tx_salaries.factories import (OrganizationFactory, PostFactory,
                                    MembershipFactory, EmployeeFactory)
 
 
-class EvenEmployeeBreakdownMedianTest(TestCase):
-    def test_update_cohort(self):
+class EvenEmployeeMedianTest(TestCase):
+    def test_gender_breakdown(self):
         parent_org = OrganizationFactory(name="Test Parent Organization")
         department = OrganizationFactory(name="Test Organization",
                                            parent=parent_org)
@@ -22,12 +22,29 @@ class EvenEmployeeBreakdownMedianTest(TestCase):
                                        position=membership_one)
         employee_two = EmployeeFactory(compensation=62217,
                                        position=membership_two)
-
         management.call_command('denormalize_salary_data')
-
         # assert median salary of the organization is 98608.5
         self.assertEqual(
             float(department.stats.female['median_paid']), 98608.5)
 
         # and the total number of female employees is 2
         self.assertEqual(department.stats.female['total_number'], 2)
+
+    def test_department_stats(self):
+        parent_org = OrganizationFactory(name="Test Parent Organization")
+        department = OrganizationFactory(name="Test Organization",
+                                           parent=parent_org)
+        post = PostFactory(organization=department)
+        # POST MUST HAVE UNICODE VALUE
+        membership_one = MembershipFactory(post=post, organization=department,
+                                           person__gender='F')
+        membership_two = MembershipFactory(post=post, organization=department,
+                                           person__gender='F')
+
+        # create two employees
+        employee_one = EmployeeFactory(compensation=135000,
+                                       position=membership_one)
+        employee_two = EmployeeFactory(compensation=62217,
+                                       position=membership_two)
+        management.call_command('denormalize_salary_data')
+        self.assertEqual(department.stats.median_paid, 98608.5)
