6f968a4aa4048163dd55f927a32da2477cd8c1ff
danhillreports <danhillreports@gmail.com>
2014-06-30 10:38:04 -0500

    Index slugs to reduce search page queries

--- tx_salaries/search_indexes.py
+++ tx_salaries/search_indexes.py
@@ -1,5 +1,4 @@
 from haystack import indexes
-from tx_people.models import Organization
 from tx_salaries.models import Employee
 
 
@@ -8,8 +7,11 @@ class EmployeeIndex(indexes.SearchIndex, indexes.Indexable):
     content_auto = indexes.EdgeNgramField(model_attr='position__person__name')
     compensation = indexes.FloatField(model_attr='compensation', null=True)
     title = indexes.CharField(model_attr='title__name', faceted=True)
+    title_slug = indexes.CharField(model_attr='title__stats__slug', faceted=True)
     department = indexes.CharField(model_attr='position__organization__name', faceted=True)
+    department_slug = indexes.CharField(model_attr='position__organization__stats__slug')
     entity = indexes.CharField(model_attr='position__organization__parent__name', faceted=True)
+    entity_slug = indexes.CharField(model_attr='position__organization__parent__stats__slug')
 
     def get_model(self):
         return Employee
