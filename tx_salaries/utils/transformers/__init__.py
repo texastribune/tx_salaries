c09eedd4e6389ceae1bde9330379b062d65748e9
Ryan Murphy <github@accounts.rdmurphy.org>
2015-01-04 22:19:49 -0600

    Add Tarrant County

--- tx_salaries/utils/transformers/__init__.py
+++ tx_salaries/utils/transformers/__init__.py
@@ -12,6 +12,7 @@ from . import north_east_isd
 from . import pasadena_isd
 from . import potter_county
 from . import state_of_texas
+from . import tarrant_county
 from . import texas_am_system
 from . import texas_tech_system
 from . import texas_tech_university
@@ -45,6 +46,7 @@ TRANSFORMERS = {
     '2252263014de05fa4efd240e0a3716c484495a93': [pasadena_isd.transform, ],
     '0e027f3d0038d81f570cbda2aeedf3dd1f720881': [potter_county.transform, ],
     '690fc3890e8f893a7b1634de2df958846ad12a1b': [state_of_texas.transform, ],
+    '96c50536164eeecade2194082544648d3bb4fd97': [tarrant_county.transform, ],
     'eb4e22c07597829d7b62f48e6f69dd2d29bf69e2': [texas_am_system.transform, ],
     'bc023bcf9e573ecdf8335ebafb24feec596e473f': [texas_tech_system.transform, ],
     'c60489252da989ab04f6a47a86265dc047dd367b': [texas_tech_university.transform, ],
