3369b0a26bff82525fc8256403d363cdfe25efe5
Ryan Murphy <github@accounts.rdmurphy.org>
2015-02-18 20:05:34 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ for dirpath, dirnames, filenames in os.walk('tx_salaries'):
 
 setup(
     name='tx_salaries',
-    version='0.9.0',
+    version='0.10.0',
     description='Texas Tribune: tx_salaries',
     author='Tribune Tech',
     author_email='tech@texastribune.org',
