fef30d9aac45c4231c357017a5121775822793fb
jolie_sky <jolie.mccullough@gmail.com>
2015-06-30 11:51:34 -0500

    trying to figure out issue with importing

--- tx_salaries/utils/transformers/ut_medical_branch.py
+++ tx_salaries/utils/transformers/ut_medical_branch.py
@@ -9,33 +9,30 @@ class TransformedRecord(mixins.GenericCompensationMixin,
         mixins.MembershipMixin, mixins.OrganizationMixin, mixins.PostMixin,
         mixins.RaceMixin, mixins.LinkMixin, base.BaseTransformedRecord):
     MAP = {
-        'last_name': 'FAMILY_NAME',
-        'first_name': 'GIVEN_NAME',
+        'last_name': 'FAMILY NAME',
+        'first_name': 'GIVEN NAME',
         'department': 'DEPARTMENT',
         'job_title': 'JOBTITLE',
         'gender': 'GENDER',
         'race': 'RACE/ETHNICITY',
-        'hire_date': 'LAST_HIRE_DT',
-        'compensation': 'ANNUAL_PAY',
-        'longevity': 'ANNUALIZED_LONGEVITY',
+        'hire_date': 'LAST HIRE DATE',
+        'compensation': 'ANNUAL PAY',
+        'longevity': 'ANNUALIZED LONGEVITY',
+        'employee_type': 'FULL/PART TIME',
     }
 
     NAME_FIELDS = ('first_name', 'last_name', )
 
-    gender_map = {'Female': 'F', 'Male': 'M'}
+    # gender_map = {'Female': 'F', 'Male': 'M'}
 
     ORGANIZATION_NAME = 'The University of Texas Medical Branch at Galveston'
 
-    # TODO current app uses University Hospital
     ORGANIZATION_CLASSIFICATION = 'University Hospital'
 
-    # TODO not given on spreadsheet, but they appear to give part time
-    compensation_type = 'FT'
-    description = 'Annual compensation'
+    DATE_PROVIDED = date(2015, 6, 24)
 
-    DATE_PROVIDED = date(2014, 1, 27)
-
-    URL = 'http://raw.texastribune.org.s3.amazonaws.com/ut_medical_branch/salaries/2014-01/Texas%20Tribune%20Salary%20PIA.xlsx'
+    URL = 'http://raw.texastribune.org.s3.amazonaws.com/ut_medical_branch/'
+        'salaries/2015-06/ut_medical_galveston.xlsx'
 
     @property
     def is_valid(self):
@@ -48,17 +45,35 @@ class TransformedRecord(mixins.GenericCompensationMixin,
             'family_name': self.last_name,
             'given_name': self.first_name,
             'name': self.get_raw_name(),
+            'gender': self.gender.strip()
         }
-        try:
-            data.update({
-                'gender': self.gender_map[self.gender.strip()]
-            })
-            return data
-        except KeyError:
-            return data
+        return data
+        # try:
+        #     data.update({
+        #         'gender': self.gender_map[self.gender.strip()]
+        #     })
+        #     return data
+        # except KeyError:
+        #     return data
+
+    @property
+    def compensation_type(self):
+
+        if self.employee_type == 'Part-time':
+            return 'PT'
+        else:
+            return 'FT'
+
+    @property
+    def description(self):
+
+        if self.employee_type == 'Part-time':
+            return "Part-time annual compensation"
+        else:
+            return "Annual compensation"
 
     def process_compensation(self):
-        #TODO longevity is in addition to base annual_pay, add if applicable
+        #longevity is in addition to base annual_pay, add if applicable
         if self.longevity.strip() == '0':
             return self.compensation
         else:
