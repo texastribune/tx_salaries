85c1d8e8c7451889eea4102ea3d644f7cc5831b1
jolie_sky <jolie.mccullough@gmail.com>
2015-07-03 16:13:35 -0500

    started setting up new transformer

--- tx_salaries/utils/transformers/ut_austin.py
+++ tx_salaries/utils/transformers/ut_austin.py
@@ -5,9 +5,6 @@ from . import mixins
 
 from .. import cleaver
 
-# http://raw.texastribune.org.s3.amazonaws.com/ut_austin/salaries/2014-02/TexasTribuneUTAustinSalaryData02-11-14.xlsx
-
-
 class TransformedRecord(mixins.GenericCompensationMixin,
         mixins.GenericDepartmentMixin, mixins.GenericIdentifierMixin,
         mixins.GenericJobTitleMixin, mixins.GenericPersonMixin,
@@ -34,10 +31,6 @@ class TransformedRecord(mixins.GenericCompensationMixin,
     ORGANIZATION_NAME = 'University of Texas at Austin'
 
     ORGANIZATION_CLASSIFICATION = 'University'
-
-    # TODO not given, 29 < 4000
-    compensation_type = 'FT'
-    description = 'Annual compensation'
 
     DATE_PROVIDED = date(2015, 6, 29)
 
@@ -70,32 +63,44 @@ class TransformedRecord(mixins.GenericCompensationMixin,
         return "-".join([year, month, day])
 
     @property
-    def get_raw_name(self):
-        # TODO include suffix
-        if self.middle_name.strip() == '':
-            self.NAME_FIELDS = ('first_name', 'last_name')
-        name_fields = [getattr(self, a).strip() for a in self.NAME_FIELDS]
-        return u' '.join(name_fields)
+    def compensation_type(self):
+        employee_type = self.employee_type
+
+        if employee_type == 'FULL TIME':
+            return 'FT'
+
+        if employee_type == 'PART TIME':
+            return 'PT'
+
+        return 'FT'
+
+    @property
+    def description(self):
+        employee_type = self.employee_type
+
+        if employee_type == 'FULL TIME':
+            return "Annual salary"
+
+        if employee_type == 'PART TIME':
+            return "Part-time annual salary"
+
+        return "Annual salary"
 
     @property
     def person(self):
-        data = {
-            'family_name': self.last_name.strip(),
-            'given_name': self.first_name.strip(),
-            'name': self.get_raw_name,
+        name = self.get_name()
+        r = {
+            'family_name': name.last,
+            'given_name': name.first,
+            'name': unicode(name),
         }
-        try:
-            gender = self.gender.strip()
-            if gender == '':
+        if self.is_mapped_value('gender'):
+            gender = self.get_mapped_value('gender')
+            if gender.split() == '':
                 gender = 'Not given'
-            else:
-                gender = self.gender_map[gender]
-            data.update({
-                'gender': gender
-            })
-            return data
-        except KeyError:
-            return data
+            r['gender'] = gender
+
+        return r
 
     def calculate_tenure(self, hire_date):
         try:
@@ -105,10 +110,10 @@ class TransformedRecord(mixins.GenericCompensationMixin,
         hire_date = date(hire_date_data[0], hire_date_data[1],
                          hire_date_data[2])
         tenure = float((self.DATE_PROVIDED - hire_date).days) / float(360)
-        if tenure < 0:
-            error_msg = ("An employee was hired after the data was provided.\n"
-                         "Is DATE_PROVIDED correct?")
-            raise ValueError(error_msg)
+        # if tenure < 0:
+        #     error_msg = ("An employee was hired after the data was provided.\n"
+        #                  "Is DATE_PROVIDED correct?")
+        #     raise ValueError(error_msg)
         return tenure
 
     @property
