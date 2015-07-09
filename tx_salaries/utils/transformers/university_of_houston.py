f5c72eb280cdef30b7f033e1a424dfe438c4500c
becca <brickaa@users.noreply.github.com>
2015-07-09 12:56:21 -0500

    it works!

--- tx_salaries/utils/transformers/university_of_houston.py
+++ tx_salaries/utils/transformers/university_of_houston.py
@@ -55,7 +55,7 @@ class TransformedRecord(
     DATE_PROVIDED = date(2015, 4, 30)
     # Y/M/D agency provided the data
     URL = ('http://raw.texastribune.org.s3.amazonaws.com/university_houston/'
-           'salaries/2015-05/Texas%20Tribune.csv')
+           'salaries/2015-05/TexasTribune_UH_Salaries.csv')
 
     @property
     def is_valid(self):
@@ -68,19 +68,13 @@ class TransformedRecord(
             'name': self.race_map[self.nationality.strip()]
         }
 
-    def process_hire_date(self, hire_date):
-        # TODO five people don't have hire dates given
-        year = hire_date[0:4]
-        month = hire_date[4:6]
-        day = hire_date[6:8]
-        return "-".join([year, month, day])
+    def calculate_tenure(self):
+        hire_date_data = map(int, self.hire_date.split('-'))
+        hire_year = hire_date_data[0]
 
-    def calculate_tenure(self, hire_date):
-        try:
-            hire_date_data = map(int, hire_date.split('/'))
-        except:
-            return None
-        hire_date = date(hire_date_data[0], hire_date_data[1],
+        if hire_year > 2015:
+            hire_year = hire_year-100
+        hire_date = date(hire_year, hire_date_data[1],
                          hire_date_data[2])
         tenure = float((self.DATE_PROVIDED - hire_date).days) / float(360)
         if tenure < 0:
@@ -88,27 +82,6 @@ class TransformedRecord(
                          "Is DATE_PROVIDED correct?")
             raise ValueError(error_msg)
         return tenure
-
-    @property
-    def compensations(self):
-        hire_date = self.process_hire_date(self.hire_date)
-        return [
-            {
-                'tx_salaries.CompensationType': {
-                    'name': self.compensation_type,
-                    'description': self.description
-                },
-                'tx_salaries.Employee': {
-                    'hire_date': hire_date,
-                    'compensation': self.compensation,
-                    'tenure': self.calculate_tenure(hire_date),
-                },
-                'tx_salaries.EmployeeTitle': {
-                    'name': unicode(
-                        cleaver.DepartmentNameCleaver(self.job_title).parse())
-                },
-            }
-        ]
 
     def get_raw_name(self):
         split_name = self.full_name.split(',')
