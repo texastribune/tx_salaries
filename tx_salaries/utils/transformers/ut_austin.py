41feeeb91bcd49728f1e74ccd9fd7a82c4bfe4f8
jolie_sky <jolie.mccullough@gmail.com>
2015-09-02 12:27:18 -0500

    finally works

--- tx_salaries/utils/transformers/ut_austin.py
+++ tx_salaries/utils/transformers/ut_austin.py
@@ -3,11 +3,13 @@ from datetime import date
 from . import base
 from . import mixins
 
-class TransformedRecord(mixins.GenericCompensationMixin,
-        mixins.GenericDepartmentMixin, mixins.GenericIdentifierMixin,
-        mixins.GenericJobTitleMixin, mixins.GenericPersonMixin,
-        mixins.MembershipMixin, mixins.OrganizationMixin, mixins.PostMixin,
-        mixins.LinkMixin, base.BaseTransformedRecord):
+class TransformedRecord(
+    mixins.GenericCompensationMixin,
+    mixins.GenericDepartmentMixin, mixins.GenericIdentifierMixin,
+    mixins.GenericJobTitleMixin, mixins.GenericPersonMixin,
+    mixins.MembershipMixin, mixins.OrganizationMixin, mixins.PostMixin,
+    mixins.RaceMixin, mixins.LinkMixin, base.BaseTransformedRecord):
+
     MAP = {
         'last_name': 'NAME LAST',
         'first_name': 'NAME FIRST',
@@ -22,7 +24,7 @@ class TransformedRecord(mixins.GenericCompensationMixin,
         'compensation': 'SALARY (FY ALLOCATION)',
     }
 
-    gender_map = {'FEMALE': 'F', 'MALE': 'M'}
+    gender_map = {u'FEMALE': u'F', u'MALE': u'M', u'': u'Unknown'}
 
     NAME_FIELDS = ('first_name', 'middle_name', 'last_name', )
 
@@ -39,14 +41,22 @@ class TransformedRecord(mixins.GenericCompensationMixin,
         # Adjust to return False on invalid fields.  For example:
         return self.last_name.strip() != ''
 
-    def process_hire_date(self, hire_date):
-        #19 cases
+    @property
+    def hire_date(self):
+        hire_date = self.get_mapped_value('hire_date')
         if hire_date.strip() == "":
             return ""
         year = hire_date[0:4]
         month = hire_date[4:6]
         day = hire_date[6:8]
         return "-".join([year, month, day])
+
+    @property
+    def gender(self):
+        sex = self.gender_map[self.get_mapped_value('gender')]
+        if sex.strip() == "":
+            return ""
+        return sex.strip()
 
     @property
     def compensation_type(self):
@@ -74,45 +84,12 @@ class TransformedRecord(mixins.GenericCompensationMixin,
         r = {
             'family_name': name.last,
             'given_name': name.first,
+            'additional_name': name.middle,
             'name': unicode(name),
-            'gender': self.gender_map[self.gender.strip()],
+            'gender': self.gender,
         }
-        print r
+
         return r
-
-    def calculate_tenure(self, hire_date):
-        try:
-            hire_date_data = map(int, hire_date.split('-'))
-        except:
-            return None
-        hire_date = date(hire_date_data[0], hire_date_data[1],
-                         hire_date_data[2])
-        tenure = float((self.DATE_PROVIDED - hire_date).days) / float(360)
-        # if tenure < 0:
-        #     error_msg = ("An employee was hired after the data was provided.\n"
-        #                  "Is DATE_PROVIDED correct?")
-        #     raise ValueError(error_msg)
-        return tenure
-
-    @property
-    def compensations(self):
-        hire_date = self.process_hire_date(self.hire_date)
-        return [
-            {
-                'tx_salaries.CompensationType': {
-                    'name': self.compensation_type,
-                    'description': self.description
-                },
-                'tx_salaries.Employee': {
-                    'hire_date': hire_date,
-                    'compensation': self.compensation,
-                    'tenure': self.calculate_tenure(hire_date),
-                },
-                'tx_salaries.EmployeeTitle': {
-                    'name': self.job_title,
-                },
-            }
-        ]
 
     @property
     def race(self):
@@ -120,5 +97,15 @@ class TransformedRecord(mixins.GenericCompensationMixin,
         if race == '':
             race = 'Not given'
         return {'name': race}
+
+
+    def calculate_tenure(self):
+        hire_date_data = map(int, self.hire_date.split('-'))
+        hire_date = date(hire_date_data[0], hire_date_data[1],
+                         hire_date_data[2])
+        tenure = float((self.DATE_PROVIDED - hire_date).days) / float(360)
+        if tenure < 0:
+            tenure = 0
+        return tenure
 
 transform = base.transform_factory(TransformedRecord)
