368d5ca39266b4a454e53c017d5138587941ce2b
jolie_sky <jolie.mccullough@gmail.com>
2015-10-08 22:37:33 -0500

    swapped old has with newly generated one

--- tx_salaries/utils/transformers/__init__.py
+++ tx_salaries/utils/transformers/__init__.py
@@ -87,6 +87,6 @@ TRANSFORMERS = {
     'a70e63dd99026dd7ca0d9c5cd6a17fea15f261a7': [ut_md_anderson.transform, ],
     '258ae8c47dc5b5bd842fb82569b1be7ba8c1bd7d': [ut_medical_branch.transform, ],
     'bf2d8d6cad54567327b44050c507096885f925b7': [ut_pan_american.transform, ],
-    '3eede716c96db3c302eaa2b9b3af6e176daf7727': [ut_san_antonio.transform, ],
+    'fd66132338a69d2fb76faa1d91b6f0d21ffc5aec': [ut_san_antonio.transform, ],
     '068d88f0ea10973339f5a4d57c8d806686d2df40': [ut_tyler.transform, ],
 }
