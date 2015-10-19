e1c59c44851b6d32a41a28b0bebb08eb75210e01
becca <brickaa@users.noreply.github.com>
2015-10-19 10:59:08 -0500

    add spring_isd hash, file

--- tx_salaries/utils/transformers/__init__.py
+++ tx_salaries/utils/transformers/__init__.py
@@ -19,6 +19,7 @@ from . import pasadena_isd
 from . import potter_county
 from . import san_antonio
 from . import rockwall_isd
+from . import spring_isd
 from . import state_of_texas
 from . import tarrant_county
 from . import texas_am_system
@@ -65,6 +66,7 @@ TRANSFORMERS = {
     '0e027f3d0038d81f570cbda2aeedf3dd1f720881': [potter_county.transform, ],
     'f53125485535c4619b16838160dc4bd41875389f': [san_antonio.transform, ],
     '5034d589091e79d58ee3c51766bd6ab05f08d3a5': [rockwall_isd.transform, ],
+    'a590b77faf9bd59210e3e1a75b55c7379d57818c': [spring_isd.transform, ],
     '690fc3890e8f893a7b1634de2df958846ad12a1b': [state_of_texas.transform, ],
     '96c50536164eeecade2194082544648d3bb4fd97': [tarrant_county.transform, ],
     'e1af1d516d482dc89008704156ed2cc4e40ea18e': [texas_am_system.transform, ],
