78f2b9cbe3c5280d3a6e05c052ae81ada37892ff
becca <brickaa@users.noreply.github.com>
2015-10-23 17:05:21 -0500

    working transformer + update to errors

--- tx_salaries/utils/transformers/cypress_fairbanks_isd.py
+++ tx_salaries/utils/transformers/cypress_fairbanks_isd.py
@@ -94,4 +94,13 @@ class TransformedRecord(
 
         return r
 
+    def calculate_tenure(self):
+        hire_date_data = map(int, self.hire_date.split('-'))
+        hire_date = date(hire_date_data[0], hire_date_data[1],
+                         hire_date_data[2])
+        tenure = float((self.DATE_PROVIDED - hire_date).days) / float(360)
+        if tenure < 0:
+            tenure = 0
+        return tenure
+
 transform = base.transform_factory(TransformedRecord)
