c8cec00d6c87024bb12aa109f857bfd92264f2f9
jolie_sky <jolie.mccullough@gmail.com>
2015-10-27 15:42:14 -0500

    made invalid any who have - as compensation

--- tx_salaries/utils/transformers/ut_southwestern.py
+++ tx_salaries/utils/transformers/ut_southwestern.py
@@ -37,7 +37,8 @@ class TransformedRecord(
     @property
     def is_valid(self):
         # Adjust to return False on invalid fields.  For example:
-        return self.last_name.strip() != ''
+        return self.last_name.strip() != '' && self.compensation.strip() != '-'
+
 
     @property
     def hire_date(self):
@@ -58,25 +59,25 @@ class TransformedRecord(
             tenure = 0
         return tenure
 
-    @property
-    def compensation_type(self):
-        employee_type = self.employee_type
-
-        if employee_type == 'FULL TIME':
-            return 'FT'
-
-        if employee_type == 'PART TIME':
-            return 'PT'
-
-    @property
-    def description(self):
-        employee_type = self.employee_type
-
-        if employee_type == 'FULL TIME':
-            return "Annual salary"
-
-        if employee_type == 'PART TIME':
-            return "Part-time annual salary"
+    # @property
+    # def compensation_type(self):
+    #     employee_type = self.employee_type
+    #
+    #     if employee_type == 'FULL TIME':
+    #         return 'FT'
+    #
+    #     if employee_type == 'PART TIME':
+    #         return 'PT'
+    #
+    # @property
+    # def description(self):
+    #     employee_type = self.employee_type
+    #
+    #     if employee_type == 'FULL TIME':
+    #         return "Annual salary"
+    #
+    #     if employee_type == 'PART TIME':
+    #         return "Part-time annual salary"
 
     @property
     def person(self):
@@ -90,22 +91,5 @@ class TransformedRecord(
         }
 
         return r
-
-    @property
-    def race(self):
-        race = self.given_race.strip()
-        if race == '':
-            race = 'UNKNOWN'
-        return {'name': race}
-
-
-    def calculate_tenure(self):
-        hire_date_data = map(int, self.hire_date.split('-'))
-        hire_date = date(hire_date_data[0], hire_date_data[1],
-                         hire_date_data[2])
-        tenure = float((self.DATE_PROVIDED - hire_date).days) / float(360)
-        if tenure < 0:
-            tenure = 0
-        return tenure
 
 transform = base.transform_factory(TransformedRecord)
