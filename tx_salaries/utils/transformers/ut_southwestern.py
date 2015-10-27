06db6725662a2ce95abbcf47f98fcb44f01e55d6
jolie_sky <jolie.mccullough@gmail.com>
2015-10-27 15:32:05 -0500

    changed tenure for future employees

--- tx_salaries/utils/transformers/ut_southwestern.py
+++ tx_salaries/utils/transformers/ut_southwestern.py
@@ -31,7 +31,8 @@ class TransformedRecord(
 
     DATE_PROVIDED = date(2015, 8, 31)
 
-    URL = 'http://s3.amazonaws.com/raw.texastribune.org/ut_southwestern_medical/salaries/2015-08/ut_southwestern.xlsx'
+    URL = ('http://s3.amazonaws.com/raw.texastribune.org/ut_southwestern_medical'
+        '/salaries/2015-08/ut_southwestern.xlsx')
 
     @property
     def is_valid(self):
@@ -47,6 +48,15 @@ class TransformedRecord(
         month = hire_date[4:6]
         day = hire_date[6:8]
         return "-".join([year, month, day])
+
+    def calculate_tenure(self):
+        hire_date_data = map(int, self.hire_date.split('-'))
+        hire_date = date(hire_date_data[0], hire_date_data[1],
+                         hire_date_data[2])
+        tenure = float((self.DATE_PROVIDED - hire_date).days) / float(360)
+        if tenure < 0:
+            tenure = 0
+        return tenure
 
     @property
     def compensation_type(self):
