3bccce5d6ee982cd530dbd76a70e325032cbe36a
Ryan Murphy <rdmurphy@users.noreply.github.com>
2016-04-26 20:38:06 -0500

    Forgot to bump that again

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ for dirpath, dirnames, filenames in os.walk('tx_salaries'):
 
 setup(
     name='tx_salaries',
-    version='1.0.1',
+    version='1.1.1',
     description='Texas Tribune: tx_salaries',
     author='Tribune Tech',
     author_email='tech@texastribune.org',
