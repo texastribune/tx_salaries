cc1679f564e963c6b4955bf54347a53b8b53215c
jolie_sky <jolie.mccullough@gmail.com>
2017-02-02 17:16:10 -0600

    udpated for this years data

--- tx_salaries/utils/transformers/texas_state_university.py
+++ tx_salaries/utils/transformers/texas_state_university.py
@@ -15,23 +15,22 @@ class TransformedRecord(
         mixins.RaceMixin, mixins.LinkMixin, base.BaseTransformedRecord):
 
     MAP = {
-        # 'last_name': '',
-        # 'first_name': '',
-        # 'middle_name': '', if needed
-        'full_name': 'Name',
+        'last_name': 'Last name',
+        'first_name': 'First name',
+        'middle_name': 'Middle name',
         # 'suffix': '', if needed
         'department': 'Organizational Unit',
         'job_title': 'Job Title',
         'hire_date': 'Hire Date',
-        'compensation': 'ANNUAL Calculation',
-        'compensation_key': 'Compensation Key',
+        'compensation': 'Annual',
+        'employee_status': 'Personnel subarea',
         'gender': 'Gender',
         'race': 'Ethnicity',
     }
 
     # The order of the name fields to build a full name.
     # If `full_name` is in MAP, you don't need this at all.
-    # NAME_FIELDS = ('first_name', 'middle_name', 'last_name', )
+    NAME_FIELDS = ('first_name', 'middle_name', 'last_name', )
 
     # The name of the organization this WILL SHOW UP ON THE SITE,
     # so double check it!
@@ -42,16 +41,13 @@ class TransformedRecord(
     # double check against salaries.texastribune.org
     ORGANIZATION_CLASSIFICATION = 'University'
 
-    # ???
-    compensation_type = 'FT'
-
     # When did you receive the data? NOT when we added it to the site.
-    DATE_PROVIDED = date(2015, 4, 8)
+    DATE_PROVIDED = date(2017, 2, 2)
 
     # The URL to find the raw data in our S3 bucket.
     URL = ('http://raw.texastribune.org.s3.amazonaws.com/'
-           'texas_state_university/salaries/2015-04/'
-           'Texas_State_University04082015.xlsx')
+           'texas_state_university/salaries/2017-02/'
+           'texas-state.xlsx')
 
     # How do they track gender? We need to map what they use to `F` and `M`.
     gender_map = {'Female': 'F', 'Male': 'M'}
@@ -61,23 +57,25 @@ class TransformedRecord(
     @property
     def is_valid(self):
         # Adjust to return False on invalid fields.  For example:
-        return self.full_name.strip() != ''
+        return self.last_name.strip() != ''
 
     @property
-    def compensation(self):
-        if not self.get_mapped_value('compensation'):
-            return 0
-        return self.get_mapped_value('compensation')
+    def compensation_type(self):
+        employee_type = self.employee_status.strip()
+
+        if 'PT' in employee_type:
+            return 'PT'
+        elif 'FT' in employee_type:
+            return 'FT'
 
     @property
     def description(self):
-        key = self.get_mapped_value('compensation_key')
+        employee_type = self.employee_status.strip()
 
-        if key == 'ANNUAL 9 Month Calculation':
-            return 'Annualized 9-month salary'
-
-        if key == 'ANNUAL 12 Month Calculation':
-            return 'Annualized 12 month salary'
+        if 'PT' in employee_type:
+            return "Part-time annual pay"
+        elif 'FT' in employee_type:
+            return "Annual pay"
 
     @property
     def person(self):
@@ -88,14 +86,10 @@ class TransformedRecord(
             'given_name': name.first,
             'additional_name': name.middle,
             'name': unicode(name),
-            'gender': self.gender_map[self.gender.strip()]
+            'gender': self.gender_map[self.get_mapped_value('gender')]
         }
 
         return r
-
-    def get_name(self):
-        return cleaver.EmployeeNameCleaver(
-            self.get_mapped_value('full_name')).parse()
 
 
 transform = base.transform_factory(TransformedRecord)
