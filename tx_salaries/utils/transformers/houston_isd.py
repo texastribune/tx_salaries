2bf6289b12a2fecd0eda2287c4ba6724e8248ba2
csessig86 <csessig86@gmail.com>
2017-05-30 15:10:38 -0500

    commit Houston ISD

--- tx_salaries/utils/transformers/houston_isd.py
+++ tx_salaries/utils/transformers/houston_isd.py
@@ -82,3 +82,42 @@ class TransformedRecord(
             return 'Hourly wage'
 
 transform = base.transform_factory(TransformedRecord)
+=======
+import re
+from datetime import date
+
+class TransformedRecord(mixins.GenericCompensationMixin,
+        mixins.GenericDepartmentMixin, mixins.GenericIdentifierMixin,
+        mixins.GenericJobTitleMixin, mixins.GenericPersonMixin,
+        mixins.MembershipMixin, mixins.OrganizationMixin, mixins.PostMixin,
+        mixins.RaceMixin, mixins.LinkMixin, base.BaseTransformedRecord):
+
+    MAP = {
+        'name': 'Name',
+        'department': 'DeptName',
+        'job_title': 'Title',
+        'compensation_type': 'Full-Time Part-Time',
+        'hire_date': 'Last Hire Date',
+        'compensation': 'Annual Salary /Hourly Rate',
+        'gender': 'Gender',
+        'race': 'Category',
+    }
+
+    NAME_FIELDS = ('name')
+
+    ORGANIZATION_NAME = 'Houston ISD'
+
+    ORGANIZATION_CLASSIFICATION = 'School District'
+
+    DATE_PROVIDED = date(2015, 10, 6)
+
+    URL = 'http://raw.texastribune.org.s3.amazonaws.com/houston_isd/houston-2015-10-06.xlsx'
+
+    @property
+    def race(self):  # make race capitalization consistent
+        return self.get_mapped_value('race').title()
+
+    @property
+    def description(self):
+        return 'N/A'
+>>>>>>> Beginning of houston_isd.py transformer. Incomplete because we need to follow up with the department.
