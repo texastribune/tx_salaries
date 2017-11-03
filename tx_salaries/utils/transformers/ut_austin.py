f0e548f7b60724d959e8524bbe95b441b456a860
csessig86 <csessig86@gmail.com>
2017-11-03 13:59:01 -0500

    init

--- tx_salaries/utils/transformers/ut_austin.py
+++ tx_salaries/utils/transformers/ut_austin.py
@@ -1,12 +1,14 @@
-from datetime import date
-
 from . import base
 from . import mixins
 
+import string
+
+from datetime import date
+
 class TransformedRecord(
     mixins.GenericCompensationMixin,
-    mixins.GenericDepartmentMixin, mixins.GenericIdentifierMixin,
-    mixins.GenericJobTitleMixin, mixins.GenericPersonMixin,
+    mixins.GenericIdentifierMixin,
+    mixins.GenericPersonMixin,
     mixins.MembershipMixin, mixins.OrganizationMixin, mixins.PostMixin,
     mixins.RaceMixin, mixins.LinkMixin, base.BaseTransformedRecord):
 
@@ -17,24 +19,27 @@ class TransformedRecord(
         'suffix_name': 'NAME SUFFIX',
         'department': 'DEPARTMENT',
         'job_title': 'TITLE',
-        'hire_date': 'CONTINUOUS EMPLOYMENT DATE',
+        'hire_date': 'HIRE DATE',
         'employee_type': 'EMPLOYMENT TYPE',
         'gender': 'GENDER',
         'given_race': 'RACE',
-        'compensation': 'SALARY (FY ALLOCATION)',
+        'compensation': 'SALARY (FISCAL YEAR ALLOCATION)',
     }
 
-    gender_map = {u'FEMALE                                            ': u'F', u'MALE                                              ': u'M', u'                                                  ': u'Unknown'}
-
-    NAME_FIELDS = ('first_name', 'middle_name', 'last_name', )
+    gender_map = {
+        'FEMALE':'F',
+        'MALE':'M',
+        '': u'Unknown'
+    }
 
     ORGANIZATION_NAME = 'University of Texas at Austin'
-
     ORGANIZATION_CLASSIFICATION = 'University'
+    NAME_FIELDS = ('first_name', 'middle_name', 'last_name', )
 
-    DATE_PROVIDED = date(2016, 6, 6)
+    DATE_PROVIDED = date(2017, 7, 5)
 
-    URL = 'http://s3.amazonaws.com/raw.texastribune.org/ut_austin/salaries/2016-06/ut_austin.xlsx'
+    URL = 'http://raw.texastribune.org.s3.amazonaws.com/'
+    'ut_austin/salaries/OPENRECORDS.ESSIG.20160705.xlsx'
 
     @property
     def is_valid(self):
@@ -42,23 +47,17 @@ class TransformedRecord(
         return self.gender.strip() != ''
 
     @property
-    def hire_date(self):
-        hire_date = self.get_mapped_value('hire_date')
-        if hire_date.strip() == "":
-            return ""
-        year = hire_date[0:4]
-        month = hire_date[4:6]
-        day = hire_date[6:8]
-        return "-".join([year, month, day])
+    def person(self):
+        name = self.get_name()
+        r = {
+            'family_name': name.last,
+            'given_name': name.first,
+            'additional_name': name.middle,
+            'name': unicode(name),
+            'gender': self.gender,
+        }
 
-    def calculate_tenure(self):
-        hire_date_data = map(int, self.hire_date.split('-'))
-        hire_date = date(hire_date_data[0], hire_date_data[1],
-                         hire_date_data[2])
-        tenure = float((self.DATE_PROVIDED - hire_date).days) / float(360)
-        if tenure < 0:
-            tenure = 0
-        return tenure
+        return r
 
     @property
     def gender(self):
@@ -88,23 +87,34 @@ class TransformedRecord(
             return "Part-time compensation"
 
     @property
-    def person(self):
-        name = self.get_name()
-        r = {
-            'family_name': name.last,
-            'given_name': name.first,
-            'additional_name': name.middle,
-            'name': unicode(name),
-            'gender': self.gender,
-        }
-
-        return r
-
-    @property
     def race(self):
         race = self.given_race.strip()
         if race == '':
             race = 'UNKNOWN'
         return {'name': race}
+
+    @property
+    def hire_date(self):
+        hire_date = self.get_mapped_value('hire_date')
+
+        if hire_date.strip() == "":
+            return ""
+
+        year = hire_date[0:4]
+        month = hire_date[4:6]
+        day = hire_date[6:8]
+
+        return "-".join([year, month, day])
+
+    def calculate_tenure(self):
+        hire_date_data = map(int, self.hire_date.split('-'))
+        hire_date = date(hire_date_data[0], hire_date_data[1],
+                         hire_date_data[2])
+        tenure = float((self.DATE_PROVIDED - hire_date).days) / float(360)
+
+        if tenure < 0:
+            tenure = 0
+
+        return tenure
 
 transform = base.transform_factory(TransformedRecord)
