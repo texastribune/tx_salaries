b763311be7e0c4dd47d0d052eeb62c96a6d2ae34
csessig86 <csessig86@gmail.com>
2017-11-06 11:58:14 -0600

    changing url again

--- tx_salaries/utils/transformers/ut_austin.py
+++ tx_salaries/utils/transformers/ut_austin.py
@@ -39,7 +39,7 @@ class TransformedRecord(
     DATE_PROVIDED = date(2017, 7, 6)
 
 
-    URL = 'http://raw.texastribune.org.s3.amazonaws.com/'
+    URL = 'https://s3.amazonaws.com/raw.texastribune.org'
     'ut_austin/salaries/2017-07/OPENRECORDS.ESSIG.20160705.xlsx'
 
     @property
