8f4726bf8fb7915b6df6ea0061f6848ff933a4f7
csessig86 <csessig86@gmail.com>
2019-07-18 09:21:05 -0500

    update version

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ for dirpath, dirnames, filenames in os.walk("tx_salaries"):
 
 setup(
     name="tx_salaries",
-    version="1.27.0",
+    version="1.28.0",
     description="Texas Tribune: tx_salaries",
     author="Tribune Tech",
     author_email="tech@texastribune.org",
