26c39a4f42134dc10b1b85822a58e4c2ee1312eb
csessig86 <csessig86@gmail.com>
2019-08-29 11:59:42 -0500

    ut austin why is this so difficult

--- tx_salaries/utils/transformers/ut_austin.py
+++ tx_salaries/utils/transformers/ut_austin.py
@@ -46,6 +46,7 @@ class TransformedRecord(
         # Adjust to return False on invalid fields.  For example:
         return self.full_name.strip() != ''
 
+
     @property
     def person(self):
         name = self.get_name()
