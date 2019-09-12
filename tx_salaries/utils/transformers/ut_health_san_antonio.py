7916107f1d2a4d5d397fd1638d80fe4be21e56d6
csessig86 <csessig86@gmail.com>
2019-09-12 13:52:19 -0500

    finish update

--- tx_salaries/utils/transformers/ut_health_san_antonio.py
+++ tx_salaries/utils/transformers/ut_health_san_antonio.py
@@ -20,7 +20,8 @@ class TransformedRecord(
         'gender': 'Sex',
         'race': 'Ethnic Grp',
         'compensation': 'Comp Rate',
-        'compensation_type': 'Full/Part'
+        'compensation_type': 'Full/Part',
+        'pay_type': 'Hourly/Salary'
     }
 
     # The order of the name fields to build a full name.
@@ -39,7 +40,13 @@ class TransformedRecord(
 
     # The URL to find the raw data in our S3 bucket.
     URL = ('https://s3.amazonaws.com/raw.texastribune.org/'
-    'ut_health_san_antonio/salaries/2017-11/325_-_Essig_TPIA_-_resp_docs.xls')
+    'ut_health_san_antonio/salaries/2019-09/454_Essig_TPIA_responsive_doc.xls')
+
+    gender_map = {
+        'Female':'F',
+        'Male':'M',
+        'Unknown': 'Unknown'
+    }
 
     @property
     def is_valid(self):
@@ -54,7 +61,7 @@ class TransformedRecord(
             'family_name': name.last,
             'given_name': name.first,
             'name': unicode(name),
-            'gender': self.gender.strip()
+            'gender': self.gender_map[self.gender.strip()]
         }
 
         return r
@@ -70,9 +77,9 @@ class TransformedRecord(
 
     @property
     def compensation_type(self):
-        emp_type = self.compensation_type
+        comp_type = self.get_mapped_value('compensation_type')
 
-        if emp_type == 'Full-Time':
+        if comp_type == 'Full-Time':
             return 'FT'
         else:
             return 'PT'
@@ -81,16 +88,25 @@ class TransformedRecord(
     @property
     def description(self):
         comp_type = self.get_mapped_value('compensation_type')
-        emp_type = self.compensation_type
+        pay_type = self.get_mapped_value('pay_type')
 
-        if comp_type == 'Salary' and emp_type == 'Full Time':
+        if pay_type == 'S' and comp_type == 'Full-Time':
             return 'Annual salary'
-        elif comp_type == 'Salary' and emp_type == 'Part Time':
+        elif pay_type == 'S' and comp_type == 'Part-Time':
             return 'Part time annual salary'
-        elif comp_type == 'Hourly' and emp_type == 'Full Time':
+        elif pay_type == 'H' and comp_type == 'Full-Time':
             return 'Full time hourly wage'
-        elif comp_type == 'Hourly' and emp_type == 'Part Time':
+        elif pay_type == 'H' and comp_type == 'Part-Time':
             return 'Part time hourly wage'
+
+    @property
+    def race(self):
+        race = self.get_mapped_value('race')
+
+        if race == '':
+            race = 'Unknown'
+
+        return {'name': race}
 
     @property
     def department(self):
